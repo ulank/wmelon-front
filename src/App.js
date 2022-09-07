58e939bf01b5c6db4bde98285f8a17b1c6c6e4d4
Ulan <kozhabekov.u@gmail.com>
2022-09-08 00:48:16 +0600

    Import MUI library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Alert, Button, Stack} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Stack spacing={2} direction="column">
+            <Button variant="contained">Bauka lox</Button>
+            <Button variant="outlined">Outlined</Button>
+            <Alert severity="error"> ОЛЖАС ГЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЙ!</Alert>
+        </Stack>
+    );
 }
 
 export default App;
